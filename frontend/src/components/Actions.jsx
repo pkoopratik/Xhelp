5650707a83d7c7fcb70ce3b3d258b18025e0ea5c
gitPK <kalaskarpratiks@gmail.com>
2024-04-15 14:02:14 +0530

    like post , reply to a post, add modal for relpying

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,8 +1,89 @@
-import { Flex } from "@chakra-ui/react";
+import {
+	Flex, Box, Text, useDisclosure, Modal, ModalBody, ModalCloseButton, ModalContent,
+	ModalFooter, ModalOverlay, ModalHeader, FormControl, Input, Button
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import useShowToast from "../hooks/useShowToast";
 
-const Actions = ({liked,setLiked}) => {
-  return (
-    <Flex gap={3} my={2} onClick={(e) => e.preventDefault()}>
+const Actions = ({ post: post_ }) => {
+
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const showToast = useShowToast();
+	const user = useRecoilValue(userAtom);
+	const [post, setPost] = useState(post_);
+	const [isLiking, setIsLiking] = useState(false);
+	const [reply, setReply] = useState("");
+	const [liked, setLiked] = useState(post_.likes.includes(user?._id));
+	const [isReplying, setIsReplying] = useState(false);
+
+	const handleLikeAndUnlike = async () => {
+
+		if (!user)
+			return showToast("Error", "You must be logged in to like the post", "error");
+		if (isLiking)
+			return;
+		setIsLiking(true);
+		try {
+			const res = await fetch("/api/posts/like/" + post._id, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			const data = await res.json();
+			if (data.error) return showToast("Error", data.error, "error");
+
+			if (!liked) {
+				setPost({ ...post, likes: [...post.likes] })
+
+			} else {
+				setPost({ ...post, likes: post.likes.filter(id => id !== user._id) })
+			}
+			setLiked(!liked)
+
+		} catch (error) {
+			showToast("Error", error.message, "error");
+		} finally {
+			setIsLiking(false);
+		}
+
+	}
+	const handleReply = async () => {
+
+		if (!user) return showToast("Error", "You must be logged in to like the post", "error");
+
+		if (isReplying) return;
+		setIsReplying(true);
+		try {
+			const res = await fetch("/api/posts/reply/" + post._id, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ text: reply })
+			});
+			const data = await res.json();
+			if (data.error)
+				return showToast("Error", data.error, "error");
+
+			setPost({ ...post, replies: [...post.replies, data.reply] });
+			showToast("Success", "Reply posted successfully", "success")
+			onClose();
+			setReply("");
+
+		} catch (error) {
+			showToast("Error", error.message, "error");
+
+		} finally {
+			setIsReplying(false);
+		}
+
+	}
+	return (
+		<Flex flexDirection="column">
+			<Flex gap={3} my={2} onClick={(e) => e.preventDefault()}>
 				<svg
 					aria-label='Like'
 					color={liked ? "rgb(237, 73, 86)" : ""}
@@ -11,7 +92,7 @@ const Actions = ({liked,setLiked}) => {
 					role='img'
 					viewBox='0 0 24 22'
 					width='20'
-					onClick={()=>setLiked(!liked)}
+					onClick={handleLikeAndUnlike}
 				>
 					<path
 						d='M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z'
@@ -28,7 +109,7 @@ const Actions = ({liked,setLiked}) => {
 					role='img'
 					viewBox='0 0 24 24'
 					width='20'
-					//onClick={onOpen}
+					onClick={onOpen}
 				>
 					<title>Comment</title>
 					<path
@@ -43,8 +124,40 @@ const Actions = ({liked,setLiked}) => {
 				<RepostSVG />
 				<ShareSVG />
 			</Flex>
+			<Flex gap={2} alignItems={"center"}>
+				<Text color={"gray.light"} fontSize={"sm"}>{post.replies.length} repies</Text>
+				<Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+				<Text color={"gray.light"} fontSize={"sm"}>{post.likes.length + (liked ? 1 : 0)} likes</Text>
 
-  )
+			</Flex>
+
+			<Modal
+				isOpen={isOpen}
+				onClose={onClose}
+			>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader></ModalHeader>
+					<ModalCloseButton />
+					<ModalBody pb={6}>
+						<FormControl>
+							<Input placeholder='Reply goes here'
+								value={reply}
+								onChange={(e) => setReply(e.target.value)} />
+						</FormControl>
+					</ModalBody>
+
+					<ModalFooter>
+						<Button colorScheme='blue' size={"sm"} mr={3}
+							onClick={handleReply} isLoading={isReplying}>
+							Reply
+						</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</Flex>
+
+	)
 }
 
 export default Actions;
--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -6,7 +6,6 @@ import useShowToast from "../hooks/useShowToast"
 import { formatDistanceToNow } from "date-fns"
 
 const Post = ({ post, postedBy }) => {
-    const [liked, setLiked] = useState(false);
     const [user, setUser] = useState(null);
     const showToast = useShowToast();
     const navigate = useNavigate()
@@ -108,14 +107,9 @@ const Post = ({ post, postedBy }) => {
                         </Box>)
                     }
                     <Flex gap={3} my={1}>
-                        <Actions liked={liked} setLiked={setLiked} />
+                        <Actions post={post} />
                     </Flex>
-                    <Flex gap={2} alignItems={"center"}>
-                        <Text color={"gray.light"} fontSize={"sm"}>{post.replies.length} repies</Text>
-                        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
-                        <Text color={"gray.light"} fontSize={"sm"}>{post.likes.length + (liked ? 1 : 0)} likes</Text>
 
-                    </Flex>
                 </Flex>
             </Flex>
         </Link>
